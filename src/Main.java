723d0c5841e322a4e2864acf110780d92fb4ed75
ashutosh-3474 <ashutosh.2201062cs@iiitbh.ac.in>
2025-12-10 15:40:54 +0530

    Added: program to calculate the distance of a line

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to line comparison program");
+
+        Main m = new Main();
+
+        Line l1 = new Line(m.getPoint(),m.getPoint());
+        l1.calculateLength();
+
+    }
+    public Point getPoint(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter x cordinate");
+        double x = sc.nextDouble();
+        System.out.println("Enter y cordinate");
+        double y = sc.nextDouble();
+        return new Point(x,y);
     }
 }
